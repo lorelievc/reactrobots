18f457bb0140968c4f840f527270296dd9f169bf
Lala <Lala@Roberts-iMac.local>
2019-08-11 19:26:29 +0800

    Updates

---
+++ precache-manifest.89d04ce46ef8bc611d6ef9a778df9ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0865452e7c09081e7b04d594a6fe7a",
+    "url": "/reactrobots/index.html"
+  },
+  {
+    "revision": "79feeca9ec8101d0a0c3",
+    "url": "/reactrobots/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "fe92ee9e3e594660e363",
+    "url": "/reactrobots/static/css/main.3bf6d905.chunk.css"
+  },
+  {
+    "revision": "79feeca9ec8101d0a0c3",
+    "url": "/reactrobots/static/js/2.9c7d66f5.chunk.js"
+  },
+  {
+    "revision": "fe92ee9e3e594660e363",
+    "url": "/reactrobots/static/js/main.899c6c7e.chunk.js"
+  },
+  {
+    "revision": "66826bc87fb96384a6cd",
+    "url": "/reactrobots/static/js/runtime~main.b5f2a438.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactrobots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
